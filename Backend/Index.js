f5122ab8fc01f9b60b080848f41559f20b0ee79b
shivanshanand <shivanshanand962@gmail.com>
2025-03-26 20:58:33 +0530

    mongo-client

--- Backend/Index.js
+++ Backend/Index.js
@@ -13,5 +13,14 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  const dbStatus =
+    mongoose.connection.readyState === 1 ? "Connected" : "Not Connected";
+  res.json({
+    message: "Welcome to Electricity Consumption Tracker",
+    database: dbStatus,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
